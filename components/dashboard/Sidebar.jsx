9dd7779042b0b2a6fba4f9a24ab49e7e58ce7556
Gaurav GC <gaurav4a4@gmail.com>
2025-06-26 19:03:23 -0400

    Update sidebar design

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,4 +1,14 @@
-import { ChevronLeft, Home, Store } from "lucide-react";
+import {
+  BaggageClaim,
+  BarChart4,
+  Cable,
+  ChevronLeft,
+  Files,
+  Home,
+  ShoppingBag,
+  ShoppingBasket,
+  Store,
+} from "lucide-react";
 import Link from "next/link";
 
 function Sidebar() {
@@ -8,25 +18,61 @@ function Sidebar() {
 
       <div className="flex flex-col">
         {/* Logo */}
-        <div className="bg-slate-950 flex space-x-2 items-center py-3 px-2">
+        {/* PR link */}
+        <Link
+          href="#"
+          className="bg-slate-950 flex space-x-2 items-center py-3 px-2"
+        >
           <Store />
           <span className="font-semibold text-xl">Inventory</span>
-        </div>
+        </Link>
         {/* Links */}
-        <nav className="flex flex-col gap-4 p-3">
-          <Link className="flex items-center space-x-2" href="">
+
+        {/* PR classname */}
+        <nav className="flex flex-col gap-3 px-3 py-6">
+          {/* PR href and classname */}
+          <Link
+            className="flex items-center space-x-2 bg-blue-600 text-slate-50 p-2 rounded-md"
+            href="#"
+          >
             <Home className="w-4 h-4" />
             <span>Home</span>
           </Link>
-          <Link className="flex items-center space-x-2" href="">
-            <Home className="w-4 h-4" />
+          {/* PR button and classname */}
+          <button className="flex items-center space-x-2 p-2">
+            {/* PR */}
+            <BaggageClaim className="w-4 h-4" />
             <span>Inventory</span>
+          </button>
+          {/* PR new addition */}
+          <button className="flex items-center space-x-2 p-2">
+            <ShoppingBasket className="w-4 h-4" />
+            <span>Sales</span>
+          </button>
+          <button className="flex items-center space-x-2 p-2">
+            {/* PR */}
+            <ShoppingBag className="w-4 h-4" />
+            <span>Purchases</span>
+          </button>
+          <Link className="flex items-center space-x-2 p-2" href="#">
+            <Cable className="w-4 h-4" />
+            <span>Integrations</span>
           </Link>
+          <Link className="flex items-center space-x-2 p-2" href="#">
+            <BarChart4 className="w-4 h-4" />
+            <span>Reports</span>
+          </Link>
+          <Link className="flex items-center space-x-2 p-2" href="#">
+            <Files className="w-4 h-4" />
+            <span>Documents</span>
+          </Link>
+          {/* PR END */}
         </nav>
       </div>
 
       {/* Bottom */}
-      <div className="flex flex-col justify-end">
+      {/* PR classname */}
+      <div className="flex flex-col justify-end items-center">
         <div className="bg-slate-950 flex space-x-2 items-center py-3 px-2">
           <ChevronLeft />
         </div>

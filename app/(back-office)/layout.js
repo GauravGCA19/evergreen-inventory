a841a3d3a578f56d44d1db21031f74a534e8920e
Mirelda <58131945+Mirelda@users.noreply.github.com>
2025-06-26 14:58:29 -0400

    Update layout for sidebar component

--- app/(back-office)/layout.js
+++ app/(back-office)/layout.js
@@ -1,11 +1,10 @@
 import Header from "@/components/dashboard/Header";
+import Sidebar from "@/components/dashboard/Sidebar";
 
 function Layout({ children }) {
   return (
     <div className="flex">
-      <div className="w-56 min-h-screen bg-slate-900 text-slate-50">
-        sidebar
-      </div>
+      <Sidebar />
       <main className="w-full bg-slate-100 min-h-screen">
         <Header />
         {children}

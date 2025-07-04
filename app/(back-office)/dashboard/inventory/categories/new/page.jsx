d726e1882b43ab92f8f89ebcf1960a602f677df6
Gaurav GC <gaurav4a4@gmail.com>
2025-07-04 00:44:11 -0400

    Creation of API end-points and form for inventory pages

--- app/(back-office)/dashboard/inventory/categories/new/page.jsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.jsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import TextInput from "@/components/formInputs/TextInput";
 import { useState } from "react";
 import SubmitButton from "@/components/formInputs/SubmitButton";
+import TextAreaInput from "@/components/formInputs/TextAreaInput";
 
 function NewCategory() {
   const {
@@ -15,14 +16,32 @@ function NewCategory() {
     formState: { errors },
   } = useForm();
   const [loading, setLoading] = useState(false);
-  function onSubmit(data) {
+  async function onSubmit(data) {
     console.log(data);
-    reset();
+    setLoading(true);
+    const baseUrl = "http://localhost:3000";
+    try {
+      const response = await fetch(`${baseUrl}/api/categories`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        console.log(response);
+        setLoading(false);
+        reset();
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   }
   return (
     <div>
       {/* Header */}
-      <FormHeader title="New Category" href="#" />
+      <FormHeader title="New Category" href="/dashboard/inventory/" />
 
       {/* Form */}
       <form
@@ -34,6 +53,36 @@ function NewCategory() {
           <TextInput
             label="Category Title"
             name="title"
+            register={register}
+            errors={errors}
+          />
+          {/* <div className="sm:col-span-2">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Category Description
+            </label>
+            
+            <div className="mt-2">
+              <textarea
+                {...register("description", { required: true })}
+                id="description"
+                name="description"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+              {errors.title && (
+                <span className="text-sm text-red-600 ">
+                  Category description is required
+                </span>
+              )}
+            </div>
+          </div> */}
+          <TextAreaInput
+            label="Category Description"
+            name="description"
             register={register}
             errors={errors}
           />
